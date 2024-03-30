c7cf21ef7c31efaa786ff318bad2e2f96f5f97ec
zonda <zondayttop1ggg@gmail.com>
2024-03-30 17:59:29 +0200

    Rewrite views

--- Restourant/views.py
+++ Restourant/views.py
@@ -1,34 +1,39 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.views import View
 from .models import Dish, DishType, Cooker, Rules
 from .forms import DishForm, RuleForm
 
-def create_dish(request):
-    if request.method == 'POST':
+class CreateDishView(View):
+    def get(self, request):
+        form = DishForm()
+        return render(request, 'create_dish.html', {'form': form})
+
+    def post(self, request):
         form = DishForm(request.POST)
         if form.is_valid():
             dish = form.save()
             return redirect('dish_list')
-    else:
-        form = DishForm()
+        return render(request, 'create_dish.html', {'form': form})
 
-    return render(request, 'create_dish.html', {'form': form})
+class DishListView(View):
+    def get(self, request):
+        dishes = Dish.objects.all()
+        return render(request, 'dish_list.html', {'dishes': dishes})
 
-def dish_list(request):
-    dishes = Dish.objects.all()
-    return render(request, 'dish_list.html', {'dishes': dishes})
+class RulesListView(View):
+    def get(self, request):
+        rules = Rules.objects.all()
+        return render(request, 'rules.html', {'rules': rules})
 
-def rules_list(request):
-    rules = Rules.objects.all()
-    return render(request, 'rules.html', {'rules': rules})
+class CreateRulesView(View):
+    def get(self, request):
+        form = RuleForm()
+        return render(request, 'create_rules.html', {'form': form})
 
-def create_rules(request):
-    if request.method == 'POST':
+    def post(self, request):
         form = RuleForm(request.POST)
         if form.is_valid():
             rule = form.save()
             return redirect('rules_list')
-    else:
-        form = RuleForm()
-
-    return render(request, 'create_rules.html', {'form': form})
+        return render(request, 'create_rules.html', {'form': form})
